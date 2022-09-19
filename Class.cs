62909bc607d0dd04ad9be9385b5666fa8a5ebe6b
Tibor Papp <mortens.4444@gmail.com>
2022-09-19 13:19:48 +0200

    Create Class.cs

---
+++ Class.cs
@@ -0,0 +1,13 @@
+// Copy to C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\ItemTemplates\CSharp\Code\1033\Class
+namespace $rootnamespace$
+{
+    public class $safeitemrootname$
+    {
+		private readonly IFoo foo;
+
+		public $safeitemrootname$(IFoo foo)
+		{
+			this.foo = foo;
+		}
+    }
+}
